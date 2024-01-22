21b905487a7c1ac04496c1de38762920435c0847
Khaled FERJANI <kferjani@linagora.com>
2024-01-22 11:17:15 +0100

    added IP service: fetch the country by ip

---
+++ registration/src/lib/services/ip/index.ts
@@ -0,0 +1,28 @@
+import { env } from '$env/dynamic/public';
+import { getUrl } from '$lib/utils/url';
+import validator from 'validator';
+import type { CountryInformation } from '../../../types';
+
+/**
+ * Returns the user country based on his IP address
+ *
+ * @param {string} ip - the IP address
+ * @returns {Promise<string>} - the user country
+ */
+export const getUserCountry = async (ip: string): Promise<string> => {
+	try {
+		if (!validator.isIP(ip)) {
+			return '';
+		}
+
+		const api = getUrl(env.PUBLIC_GEO_API_URL);
+		const response = await fetch(`${api}/ip/country/${ip}.json`);
+		const info = (await response.json()) as CountryInformation;
+
+		return info.country ?? '';
+	} catch (err) {
+		console.error('Failed to fetch the IP country', err);
+
+		return '';
+	}
+};
