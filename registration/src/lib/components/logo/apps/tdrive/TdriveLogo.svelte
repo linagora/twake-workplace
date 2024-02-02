f819e0fa1954a025f2508583b6d82f847e93f580
Khaled FERJANI <kferjani@linagora.com>
2024-02-02 14:23:26 +0100

    added application logos

---
+++ registration/src/lib/components/logo/apps/tdrive/TdriveLogo.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { LogoType } from '../../../../../types';
+	import Header from './TdriveHeader.svelte';
+	import Landing from './TdriveLanding.svelte';
+
+	export let type: LogoType = 'header';
+</script>
+
+{#if type === 'header'}
+	<Header />
+{:else}
+	<Landing />
+{/if}
---
+++ registration/src/lib/components/logo/apps/workplace/TwakeWorkplaceLogo.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { LogoType } from '../../../../../types';
+	import Header from './WorkplaceHeader.svelte';
+	import Landing from './WorkspalaceLanding.svelte';
+
+	export let type: LogoType = 'header';
+</script>
+
+{#if type === 'header'}
+	<Header />
+{:else}
+	<Landing />
+{/if}
