115a337bbe20105b24fd26be9f4310c83ed84c79
Khaled FERJANI <kferjani@linagora.com>
2024-02-06 13:56:13 +0100

    feat:#6 add confirmation component

---
+++ registration/src/lib/components/otp/PhoneConfirmed.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Confirmed from '$lib/components/icons/Confirmed.svelte';
+	import { t } from 'svelte-i18n';
+</script>
+
+<div class="flex p-[16px] flex-col items-center gap-[4px] self-stretch">
+	<Confirmed />
+	<h1 class="text-center text-[22px] not-italic font-semibold leading-7">
+		{$t('Your phone number successfully confirmed')}
+	</h1>
+</div>
