5f353ad6f10da541098a6bb8a6670673d380ef99
Khaled FERJANI <kferjani@linagora.com>
2024-02-06 14:59:28 +0100

    feat:#6 add page header component

---
+++ registration/src/lib/components/dispaly/FormHeader.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import NavigateBack from '$lib/components/button/NavigateBack.svelte';
+	import AppLogo from '$lib/components/logo/AppLogo.svelte';
+
+	export let logo: boolean = true;
+</script>
+
+<div class="flex flex-row">
+	<NavigateBack />
+	{#if logo}
+		<div class="flex p-0 flex-col justify-center items-center self-stretch w-full">
+			<div class="flex w-full p-[12px] flex-col items-center gap-[10px]">
+				<AppLogo />
+			</div>
+		</div>
+	{/if}
+</div>
