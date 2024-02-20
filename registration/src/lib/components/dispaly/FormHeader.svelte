a84bd868a32a0962fa4d7e9767f1ffe1a46985c1
Khaled FERJANI <kferjani@linagora.com>
2024-02-20 14:11:01 +0100

    🎨 Updated FormHeader component"

--- registration/src/lib/components/dispaly/FormHeader.svelte
+++ registration/src/lib/components/dispaly/FormHeader.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import NavigateBack from '$lib/components/button/NavigateBack.svelte';
 	import AppLogo from '$lib/components/logo/AppLogo.svelte';
+	import { registrationStep, rewindRegistrationStep } from '$store';
 
-	export let logo: boolean = true;
+	export let show: boolean = true;
 </script>
 
 <div class="flex flex-row">
-	<NavigateBack />
-	{#if logo}
+	<NavigateBack handler={rewindRegistrationStep} show={$registrationStep !== 'home'} />
+	{#if show}
 		<div class="flex p-0 flex-col justify-center items-center self-stretch w-full">
 			<div class="flex w-full p-[12px] flex-col items-center gap-[10px]">
 				<AppLogo />
