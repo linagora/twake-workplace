943133e38786a603dc4411614440bb6bc5fdf72f
Khaled FERJANI <kferjani@linagora.com>
2024-02-20 14:09:59 +0100

    ♻️ Set store from initial page load

--- registration/src/routes/+page.svelte
+++ registration/src/routes/+page.svelte
@@ -1,19 +1,33 @@
 <script lang="ts">
-	import { activeTab, app, form as formStore, userCountry } from './../store';
+	import {
+		activeTab,
+		app,
+		form as formStore,
+		userCountry,
+		registrationStep,
+		verified,
+		phone
+	} from './../store';
 	import AccessSelect from '$lib/components/landing/AccessSelect.svelte';
 	import Hero from '$lib/components/landing/Hero.svelte';
 	import type { ActionData, PageData } from './$types';
+	import type { CountryCode } from 'svelte-tel-input/types';
+	import type { ApplicationType } from '../types';
 
 	export let form: ActionData;
 	export let data: PageData;
+
 	$: formStore.set(form);
 	$: activeTab.set(data.isLogin ? 'login' : 'register');
-	$: userCountry.set(data.country);
-	$: app.set(data.app);
+	$: userCountry.set(data.country as CountryCode);
+	$: app.set(data.app as ApplicationType);
+	$: registrationStep.set(data.step);
+	$: verified.set(data.verified);
+	$: phone.set(data.phone ?? '');
 </script>
 
 <div class="flex w-full h-full lg:px-10 xl:px-[124px]">
-	<div class="flex flex-col lg:flex-row w-full pt-9 xl:space-x-24">
+	<div class="flex flex-col lg:flex-row w-full lg:pt-9 xl:space-x-24 h-full">
 		<Hero />
 		<AccessSelect />
 	</div>
