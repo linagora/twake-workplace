0dcf64587192e5fe07589c87d09c07454cee7588
Khaled FERJANI <kferjani@linagora.com>
2024-02-22 14:44:48 +0100

    ⚰️ Remove unused code

--- registration/src/lib/utils/email.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Validates en email address.
- *
- * @param {string} email - The email address to validate.
- * @returns {boolean} - true if valid, false otherwise
- */
-export const validateEmail = (email: string): boolean => {
-	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-};
