007b11bf9386657f2c67c718d9286bfdf67ecef1
Khaled FERJANI <kferjani@linagora.com>
2024-03-29 11:22:53 +0100

    🎨 feat: added password recovery store

--- registration/src/store/index.ts
+++ registration/src/store/index.ts
@@ -1,7 +1,8 @@
 import { writable, get } from 'svelte/store';
 import type { ActionData } from '../routes/$types';
-import type { RegistrationStepType, ApplicationType, Tab } from '$types';
+import type { RegistrationStepType, ApplicationType, Tab, PasswordRecoveryStepType } from '$types';
 import type { CountryCode } from 'svelte-tel-input/types';
+import type { ActionData as RecoveryActionData } from '../routes/recover-password/$types';
 
 export const form = writable<ActionData>();
 export const verified = writable<boolean>(false);
@@ -15,6 +16,10 @@ export const userCountry = writable<CountryCode | null>(null);
 export const app = writable<ApplicationType>('default');
 export const registrationStep = writable<RegistrationStepType>('home');
 export const showBanner = writable<boolean>(true);
+export const passwordRecoveryStep = writable<PasswordRecoveryStepType>('phone');
+export const verifiedRecovery = writable<boolean>(false);
+export const recoveryPhone = writable<string>('');
+export const recoveryForm = writable<RecoveryActionData>();
 
 verified.subscribe((v) => {
 	if (v === true) {
@@ -100,4 +105,39 @@ export const getAppName = () => {
 		: currentApp === 'tmail'
 		? 'Twake mail'
 		: 'Twake';
+};
+
+export const nextPasswordRecoveryStep = () => {
+	const currentStep = get(passwordRecoveryStep);
+
+	if (currentStep === 'phone') {
+		passwordRecoveryStep.set('otp');
+		return;
+	}
+
+	if (currentStep === 'otp') {
+		passwordRecoveryStep.set('password');
+		return;
+	}
+
+	if (currentStep === 'password') {
+		passwordRecoveryStep.set('success');
+		return;
+	}
+};
+
+export const rewindPasswordRecoveryStep = () => {
+	const currentStep = get(passwordRecoveryStep);
+
+	if (currentStep === 'phone') return;
+
+	if (currentStep === 'otp') {
+		passwordRecoveryStep.set('phone');
+		return;
+	}
+
+	if (currentStep === 'password') {
+		passwordRecoveryStep.set('otp');
+		return;
+	}
 };
