9015c5a9d875e876f6de0971ac7abab3058c95ef
Khaled FERJANI <kferjani@linagora.com>
2024-03-29 11:23:31 +0100

    🎨 feat: updated FormHeader to support password recovery steps

--- registration/src/lib/components/dispaly/FormHeader.svelte
+++ registration/src/lib/components/dispaly/FormHeader.svelte
@@ -4,15 +4,13 @@
 	import { activeTab, registrationStep, rewindRegistrationStep } from '$store';
 
 	export let show: boolean = true;
+
+	$: showNavigation =
+		$registrationStep !== 'home' && $registrationStep !== 'confirmed' && $activeTab !== 'login';
 </script>
 
 <div class="flex flex-row">
-	<NavigateBack
-		handler={rewindRegistrationStep}
-		show={$registrationStep !== 'home' &&
-			$registrationStep !== 'confirmed' &&
-			$activeTab !== 'login'}
-	/>
+	<NavigateBack handler={rewindRegistrationStep} show={showNavigation} />
 	{#if show}
 		<div class="flex p-0 flex-col justify-center items-center self-stretch w-full">
 			<div class="flex w-full p-[12px] flex-col items-center gap-[10px]">
