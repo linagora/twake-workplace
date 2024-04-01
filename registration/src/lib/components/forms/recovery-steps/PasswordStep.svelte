e83b4fa544441cba2a219fda6c332d53dab43414
Khaled FERJANI <kferjani@linagora.com>
2024-04-01 09:34:42 +0100

    ✨ feat: added recovery password step

---
+++ registration/src/lib/components/forms/recovery-steps/PasswordStep.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import PrimaryButton from '$components/button/PrimaryButton.svelte';
+	import PasswordField from '$components/input/PasswordField.svelte';
+	import { createUserFormSchema, fullCreateUserFormSchema } from '$lib/schemas/zodSchema';
+	import { t } from 'svelte-i18n';
+
+	let sendPasswordForm: HTMLFormElement;
+
+	let password = '';
+	let confirmPassword = '';
+
+	$: disabled = !fullCreateUserFormSchema.safeParse({
+		password,
+		confirmPassword
+	}).success;
+
+	const handler = () => {
+		if (disabled) return;
+
+		sendPasswordForm.requestSubmit();
+	};
+</script>
+
+<form use:enhance action="?/resetPassword" method="POST" class="hidden" bind:this={sendPasswordForm}>
+	<input type="text" name="password" bind:value={password} required />
+</form>
+
+<div class="flex flex-col px-4 lg:px-0 h-full pt-20 pb-[28px] lg:pb-6">
+	<div class="flex p-4 flex-col items-center gap-[4px] self-stretch">
+		<div class="self-stretch text-center text-zinc-900 text-[22px] font-semibold leading-7">
+			{$t('set-new-password')}
+		</div>
+		<div class="text-center text-gray-400 text-base font-normal leading-normal">
+			{$t('new-password-policy')}
+		</div>
+	</div>
+	<div class="flex py-4 flex-col gap-6 self-stretch">
+		<PasswordField
+			name="password"
+			placeholder={$t('Password')}
+			label={$t('Password')}
+			bind:value={password}
+			isInvalid={!createUserFormSchema.safeParse({ password }).success}
+			error={$t('weak_password')}
+		/>
+
+		<PasswordField
+			name="confirmpassword"
+			placeholder={$t('Confirm password')}
+			label={$t('Confirm password')}
+			bind:value={confirmPassword}
+			isInvalid={!fullCreateUserFormSchema.safeParse({ password, confirmPassword }).success}
+			error={$t('password_mismatch')}
+		/>
+	</div>
+	<div class="flex flex-col mt-auto gap-4 space-y-4 py-4">
+		<PrimaryButton ariaLabel="next" {disabled} {handler}>{$t('set-password')}</PrimaryButton>
+	</div>
+</div>
