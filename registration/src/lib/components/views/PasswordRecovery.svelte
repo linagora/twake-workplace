0fdad793e514be7fed99e54eab8a3e825120258a
Khaled FERJANI <kferjani@linagora.com>
2024-04-01 10:23:46 +0100

    🎨 chore: refactor password recovery main screen

--- registration/src/lib/components/views/PasswordRecovery.svelte
+++ registration/src/lib/components/views/PasswordRecovery.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import { passwordRecoveryStep } from '$store';
+	import { passwordRecoveryStep, rewindPasswordRecoveryStep } from '$store';
 	import FormHeader from '$components/dispaly/FormHeader.svelte';
 	import PhoneStep from '$components/forms/recovery-steps/PhoneStep.svelte';
 	import OtpStep from '$components/forms/recovery-steps/OTPStep.svelte';
 	import PasswordStep from '$components/forms/recovery-steps/PasswordStep.svelte';
+	import RecoverySuccessStep from '$components/forms/recovery-steps/RecoverySuccessStep.svelte';
+	import NavigateBack from '$components/button/NavigateBack.svelte';
+
+	$: showNavigation = $passwordRecoveryStep !== 'success';
 </script>
 
 <div
@@ -12,11 +16,13 @@
 	<div
 		class="bg-white shadow-[0px_0px_52px_0px_rgba(0,0,0,0.08)] md:rounded-3xl flex flex-col w-full lg:w-[400px] xl:w-[504px] md:px-[60px] h-full"
 	>
-		<FormHeader show={false} />
+		<NavigateBack handler={rewindPasswordRecoveryStep} show={showNavigation} />
 		{#if $passwordRecoveryStep === 'otp'}
 			<OtpStep />
-		{:else if $passwordRecoveryStep === 'password' || $passwordRecoveryStep === 'success'}
+		{:else if $passwordRecoveryStep === 'password'}
 			<PasswordStep />
+		{:else if $passwordRecoveryStep === 'success'}
+			<RecoverySuccessStep />
 		{:else}
 			<PhoneStep />
 		{/if}
