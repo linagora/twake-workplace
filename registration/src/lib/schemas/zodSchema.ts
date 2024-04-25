aedcc0398f3539524ee65e5ed71205fed852fbce
Khaled FERJANI <kferjani@linagora.com>
2024-04-25 12:46:59 +0100

    🎨 chore: updated zod schema refinement

--- registration/src/lib/schemas/zodSchema.ts
+++ registration/src/lib/schemas/zodSchema.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod';
 import validator from 'validator';
 import { validatePassword } from '$utils/password';
-import { checkUserName } from '../utils/username';
+import { validateNickName } from '$utils/username';
 
 const createUserSchema = z.object({
-	nickName: z.string().refine(checkUserName),
+	nickName: z.string().refine((val) => validateNickName(val)),
 	firstName: z.string().trim().min(1),
 	lastName: z.string().trim().min(1),
-	password: z.string().refine(validatePassword, {
+	password: z.string().refine((val) => validatePassword(val), {
 		message:
 			'Password must be at least 8 characters and contain an uppercase letter, lowercase letter, and number'
 	}),
