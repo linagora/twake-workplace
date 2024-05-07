cc33cfbf4c66ed6926e40c9a09be49d1be60fde0
Khaled FERJANI <kferjani@linagora.com>
2024-05-07 17:43:59 +0100

    💄 fix: adjust login form layout and disabled button color

--- registration/src/lib/components/buttons/SubmitButton.svelte
+++ registration/src/lib/components/buttons/SubmitButton.svelte
@@ -11,7 +11,7 @@
 	aria-label={ariaLabel}
 	{disabled}
 	class="flex items-center rounded-full justify-center w-full h-12 {disabled
-		? 'bg-disabled text-[#1C1B1F] text-opacity-[0.38]'
+		? 'bg-slate-200 text-slate-400'
 		: 'bg-primary text-white'} text-center text-sm not-italic font-medium leading-5 tracking-[0.1px] space-x-5"
 >
 	<slot />
