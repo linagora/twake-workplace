b670288cb249e188fd04a99a7997f0c06102c8b3
Khaled FERJANI <kferjani@linagora.com>
2024-05-08 17:03:55 +0100

    ✨ feat: integrate new phone field

--- registration/src/lib/components/forms/steps/registration/CheckPhoneStep.svelte
+++ registration/src/lib/components/forms/steps/registration/CheckPhoneStep.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import TelField from '$components/input/TelField.svelte';
 	import { t } from 'svelte-i18n';
 	import { userCountry, form } from '$store';
 	import PrimaryButton from '$components/buttons/PrimaryButton.svelte';
@@ -7,14 +6,10 @@
 	import type { E164Number } from 'svelte-tel-input/types';
 	import { enhance } from '$app/forms';
 	import UsedPhone from '$components/otp/UsedPhone.svelte';
-  import { PhoneInput } from 'react-international-phone';
-  import 'react-international-phone/style.css';
-	import TelFieldReact from '$src/lib/components/input/TelFieldReact.svelte';
+	import TelField from '$components/input/TelField.svelte';
 
-
-	export let value: null | E164Number = null;
-	export let valid = true;
-
+	let value: null | E164Number = null;
+	let valid = true;
 	let sendOtpForm: HTMLFormElement;
 	let loading = false;
 	let phoneChecked = false;
@@ -74,7 +69,7 @@
 		</div>
 	</div>
 	<div class="py-4">
-		<TelFieldReact
+		<TelField
 			bind:valid
 			bind:value
 			bind:selectedCountry={$userCountry}
