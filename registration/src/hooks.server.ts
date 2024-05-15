d2d3674925ecc19794f57474f8b57da017150ac7
Khaled FERJANI <kferjani@linagora.com>
2024-05-15 15:12:34 +0100

    🎨 feat: handle multiple proxy request paths

--- registration/src/hooks.server.ts
+++ registration/src/hooks.server.ts
@@ -2,7 +2,7 @@ import { env } from '$env/dynamic/private';
 import { handleSession } from 'svelte-kit-cookie-session';
 import { errorHandler } from '$utils/error';
 import { ldapConnectionHandler } from '$utils/db';
-import { handleProxy, PROXY_PATH } from '$utils/proxy';
+import { handleProxy, PROXY_PATHS } from '$utils/proxy';
 
 export const handle = handleSession(
 	{
@@ -13,8 +13,10 @@ export const handle = handleSession(
 	async ({ event, resolve }) => {
 		await ldapConnectionHandler();
 
-		if (event.url.pathname.startsWith(PROXY_PATH)) {
-			return await handleProxy({ event, resolve });
+		for (const path of PROXY_PATHS) {
+			if (event.url.pathname.startsWith(path)) {
+				return await handleProxy({ event, resolve });
+			}
 		}
 
 		return resolve(event);
--- registration/src/lib/utils/proxy.ts
+++ registration/src/lib/utils/proxy.ts
@@ -2,7 +2,8 @@ import type { Handle } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
 import logger from '$services/logger';
 
-export const PROXY_PATH = '/oauth2';
+export const PROXY_PATHS = ['/oauth2', '/.well-known'];
+export const PROXY_AUTH_PATH = '/oauth2/authorize';
 
 export const handleProxy = (async ({ event }) => {
 	const { request, url } = event;
@@ -15,6 +16,10 @@ export const handleProxy = (async ({ event }) => {
 
 	requestHeaders.delete('host');
 	requestHeaders.delete('connection');
+
+	if (url.pathname.startsWith(PROXY_AUTH_PATH)) {
+		return redirectToLogin(url.origin, proxiedUrl.toString());
+	}
 
 	try {
 		const response = await fetch(proxiedUrl.toString(), {
@@ -37,3 +42,17 @@ export const handleProxy = (async ({ event }) => {
 		throw error;
 	}
 }) satisfies Handle;
+
+/**
+ * Redirects user back to login page with post_login_redirect_url
+ *
+ * @param {string} url - the url to redirect to after login.
+ * @param {string} base - the base url.
+ * @returns {Response} - the redirect response.
+ */
+const redirectToLogin = (base: string, url: string): Response => {
+	const loginUrl = new URL(base);
+	loginUrl.searchParams.set('post_login_redirect_url', url);
+
+	return Response.redirect(loginUrl.toString(), 302);
+};
