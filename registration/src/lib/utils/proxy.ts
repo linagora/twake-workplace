a3c41193069db50b61ee262ad8adf9587cf2c653
Khaled FERJANI <kferjani@linagora.com>
2024-05-15 11:34:47 +0100

    ✨ feat: added Oath2 proxy handler

---
+++ registration/src/lib/utils/proxy.ts
@@ -0,0 +1,41 @@
+import type { Handle } from '@sveltejs/kit';
+import { env } from '$env/dynamic/private';
+import logger from '$services/logger';
+
+export const PROXY_PATH = '/oath2';
+
+export const handleProxy = (async ({ event }) => {
+	const { request, url } = event;
+
+	const PROXY_SERVER_URL = env.AUTH_URL;
+
+	const proxiedUrl = new URL(PROXY_SERVER_URL);
+	const requestHeaders = new Headers(request.headers);
+
+	proxiedUrl.pathname = url.pathname;
+	proxiedUrl.search = url.search;
+
+	requestHeaders.delete('host');
+	requestHeaders.delete('connection');
+
+	try {
+		const response = await fetch(proxiedUrl.toString(), {
+			redirect: 'manual',
+			method: request.method,
+			headers: requestHeaders
+		});
+
+		const responseHeaders = new Headers(response.headers);
+
+		responseHeaders.delete('content-encoding');
+
+		return new Response(response.body, {
+			status: response.status,
+			statusText: response.statusText,
+			headers: responseHeaders
+		});
+	} catch (error) {
+		logger.error('Could not proxy request: ', { error });
+		throw error;
+	}
+}) satisfies Handle;
