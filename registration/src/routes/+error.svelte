46bed3773f9ed759a2a1d1e3d0eb29f10b9a60ee
Khaled FERJANI <kferjani@linagora.com>
2024-05-16 10:59:36 +0100

    🎨 feat: display different 404 error message

--- registration/src/routes/+error.svelte
+++ registration/src/routes/+error.svelte
@@ -1,9 +1,9 @@
 <script>
-	import Error from '$lib/components/dispaly/Error.svelte';
-
 	import { page } from '$app/stores';
 	import { dev } from '$app/environment';
-	import Fail from '$lib/components/dispaly/Fail.svelte';
+	import Fail from '$components/dispaly/Fail.svelte';
+	import NotFound from '$components/dispaly/NotFound.svelte';
+	import InternalError from '$components/dispaly/InternalError.svelte';
 </script>
 
 <div
@@ -12,11 +12,15 @@
 	<div
 		class="bg-gray-800 text-gray-500 rounded-xl flex flex-col shadow-xl px-2 py-16 md:px-16 w-full md:w-2/3 lg:w-2/5 space-y-5"
 	>
-		<Error />
+		{#if $page.status === 404}
+			<NotFound />
+		{:else}
+			<InternalError />
+		{/if}
 
 		{#if dev}
 			<Fail>
-				{$page.status} - {$page.error?.message}
+				code: {$page.status} - {$page.error?.message}
 			</Fail>
 		{/if}
 	</div>
