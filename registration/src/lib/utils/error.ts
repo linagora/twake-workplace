5dd2ad03de7ff5a54f97bcd5685009dd42840a9e
Khaled Ferjani <k2ll33d2@gmail.com>
2024-05-16 11:24:13 +0100

    Handle 404 errors (#166)
    * ✨ feat: add error components
    * 🎨 feat: display different 404 error message
    * 🌐 feat: add 404 error message translation
    * 🎨 feat: improve HandleServerError hook
    * 🗑️ chore: remove old error component
    * 🎨 fix: typo in components display folder
    * 🎨 feat: use $components alias
    * ✏️ fix: typo in translations

--- registration/src/lib/utils/error.ts
+++ registration/src/lib/utils/error.ts
@@ -3,17 +3,24 @@ import logger from '$services/logger';
 
 export const errorHandler = (({ error, event }) => {
 	const client = event.getClientAddress();
-	const { referrer, method, url } = event.request;
+	const { referrer, method, url, headers } = event.request;
+	const typedError = error as Error & { status?: number };
+
+	if (typedError?.status === 404) {
+		logger.warn('Not found', { url });
+
+		return { message: 'Not found', status: 404 };
+	}
+
+	if (typedError.status && typedError.status >= 400 && typedError.status < 500) {
+		logger.warn('Bad Request', { url });
+
+		return { message: 'Bad Request', status: typedError.status };
+	}
 
 	logger.fatal(
 		'Server error',
-		{
-			url,
-			client,
-			method,
-			referrer,
-			userAgent: event.request.headers.get('user-agent') || 'Unknown'
-		},
+		{ url, client, method, referrer, userAgent: headers.get('user-agent') || 'Unknown' },
 		{ error }
 	);
 
