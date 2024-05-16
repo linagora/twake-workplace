eb3a724bc04e77f22e09801e4645e6d43dbe0ce6
Khaled Ferjani <k2ll33d2@gmail.com>
2024-05-16 15:26:44 +0100

    💄 fix: update the Hero style (#168)
    💄 adjust font size
    💄 adjust line height
    💄 adjust spacing

--- registration/src/lib/components/landing/Hero.svelte
+++ registration/src/lib/components/landing/Hero.svelte
@@ -6,13 +6,13 @@
 </script>
 
 <div
-	class="hidden lg:flex flex-col w-full lg:max-w-[508px] px-10 lg:px-0 space-y-6 pt-52 justify-center items-start pb-9"
+	class="hidden lg:flex flex-col w-full lg:max-w-[506px] px-10 lg:px-0 space-y-6 pt-52 justify-center items-start pb-9"
 >
-	<div class="flex flex-col h-full justify-center">
+	<div class="flex flex-col h-full justify-center space-y-[47px]">
 		<Logo />
-		<div class="flex flex-col space-y-5 w-full h-full 4xl:h-auto">
+		<div class="flex flex-col space-y-5 h-full 4xl:h-auto pr-10">
 			<h1
-				class="text-[16pt] leading-8 not-italic font-semibold lg:leading-[44px] text-center lg:text-left w-full text-disabled-text"
+				class="text-[32px] leading-10 not-italic font-semibold lg:leading-[44px] text-center lg:text-left w-full text-disabled-text"
 			>
 				{$t('welcome')}
 			</h1>
