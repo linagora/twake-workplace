e5824f643fbfed322042ab833d1b7b933827edbf
Khaled Ferjani <k2ll33d2@gmail.com>
2024-05-16 15:35:05 +0100

    💄 feat: adjust the recovery page layout (#169)

--- registration/src/lib/components/views/PasswordRecovery.svelte
+++ registration/src/lib/components/views/PasswordRecovery.svelte
@@ -9,21 +9,23 @@
 	$: showNavigation = $passwordRecoveryStep !== 'success';
 </script>
 
-<div
-	class="w-full flex flex-col md:flex-row justify-end h-full lg:max-h-[668px] large:min-h-[768px] large:max-h-[768px] 2xl:max-h-[768px]"
->
+<div class="w-full flex flex-col justify-center items-end h-full">
 	<div
-		class="bg-white shadow-[0px_0px_52px_0px_rgba(0,0,0,0.08)] md:rounded-3xl flex flex-col w-full lg:w-[400px] xl:w-[504px] md:px-[60px] h-full"
+		class="w-full flex flex-col md:flex-row justify-end h-full lg:max-h-[668px] large:min-h-[768px] large:max-h-[768px] 2xl:max-h-[768px]"
 	>
-		<NavigateBack handler={rewindPasswordRecoveryStep} show={showNavigation} />
-		{#if $passwordRecoveryStep === 'otp'}
-			<OtpStep />
-		{:else if $passwordRecoveryStep === 'password'}
-			<PasswordStep />
-		{:else if $passwordRecoveryStep === 'success'}
-			<RecoverySuccessStep />
-		{:else}
-			<PhoneStep />
-		{/if}
+		<div
+			class="bg-white shadow-[0px_0px_52px_0px_rgba(0,0,0,0.08)] md:rounded-3xl flex flex-col w-full lg:w-[400px] xl:w-[504px] md:px-[60px] h-full"
+		>
+			<NavigateBack handler={rewindPasswordRecoveryStep} show={showNavigation} />
+			{#if $passwordRecoveryStep === 'otp'}
+				<OtpStep />
+			{:else if $passwordRecoveryStep === 'password'}
+				<PasswordStep />
+			{:else if $passwordRecoveryStep === 'success'}
+				<RecoverySuccessStep />
+			{:else}
+				<PhoneStep />
+			{/if}
+		</div>
 	</div>
 </div>
--- registration/src/routes/recover-password/+page.svelte
+++ registration/src/routes/recover-password/+page.svelte
@@ -22,8 +22,8 @@
 	$: userCountry.set(country as CountryCode);
 </script>
 
-<div class="flex w-full h-full lg:px-10 xl:px-[124px] 2xl:px-52 3xl:px-72 4xl:px-96">
-	<div class="flex flex-col lg:flex-row w-full lg:pt-9 xl:space-x-24 h-full">
+<div class="flex w-full h-full lg:px-10 xl:px-[148px] 2xl:px-52 3xl:px-72 4xl:px-96">
+	<div class="flex flex-col lg:flex-row w-full xl:space-x-24 h-full">
 		<Hero />
 		<PasswordRecovery />
 	</div>
