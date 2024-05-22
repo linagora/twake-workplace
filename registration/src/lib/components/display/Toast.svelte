18b86493cea51331de3645b662accf1e9abec254
Khaled FERJANI <kferjani@linagora.com>
2024-05-22 14:57:14 +0100

    ✨ feat: added Toast component

---
+++ registration/src/lib/components/display/Toast.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	export let show: boolean = false;
+</script>
+
+<div class="absolute w-full bottom-0 mb-10 right-0 {show ? 'flex ' : 'hidden'} lg:hidden px-5">
+	<div
+		id="toast-success"
+		class="flex items-center w-full p-4 rounded-lg shadow text-gray-400 bg-gray-800"
+		role="alert"
+	>
+		<div
+			class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg bg-green-800 text-green-200"
+		>
+			<svg
+				class="w-5 h-5"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+				viewBox="0 0 20 20"
+			>
+				<path
+					d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"
+				/>
+			</svg>
+			<span class="sr-only">Check icon</span>
+		</div>
+		<div class="ms-3 text-sm font-normal">
+			<slot />
+		</div>
+		<button
+			type="button"
+			class="ms-auto -mx-1.5 -my-1.5 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 inline-flex items-center justify-center h-8 w-8 text-gray-500 hover:text-white bg-gray-800 hover:bg-gray-700"
+			data-dismiss-target="#toast-success"
+			aria-label="Close"
+			on:click={() => (show = false)}
+		>
+			<span class="sr-only">Close</span>
+			<svg
+				class="w-3 h-3"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 14 14"
+			>
+				<path
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+				/>
+			</svg>
+		</button>
+	</div>
+</div>
