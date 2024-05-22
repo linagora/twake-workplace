f4be52ac4b2d8381c177c2dfec40366a1e0ea402
Khaled FERJANI <kferjani@linagora.com>
2024-05-22 11:37:44 +0100

    🎨 chore: remove unused redirect stores

--- registration/src/store/redirect.ts
+++ registration/src/store/redirect.ts
@@ -1,5 +1,4 @@
 import { writable } from 'svelte/store';
 
 export const redirectUrl = writable<string | null>(null);
-export const challenge = writable<string | null>(null);
-export const clientId = writable<string | null>(null);
+
