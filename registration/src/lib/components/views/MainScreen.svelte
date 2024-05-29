f75a9e89c1db5756173ca1044cf411585d87b1a3
Khaled FERJANI <kferjani@linagora.com>
2024-05-29 16:03:34 +0100

    🐛 fix: desktop hero

--- registration/src/lib/components/views/MainScreen.svelte
+++ registration/src/lib/components/views/MainScreen.svelte
@@ -15,13 +15,20 @@
 	};
 </script>
 
-<div class="flex flex-col px-4 lg:px-0 h-full w-full xs:pt-[120px] pt-[188px] lg:pt-[169px] pb-[28px] lg:pb-6">
+<div
+	class="flex flex-col px-4 lg:px-0 h-full w-full xs:pt-[120px] pt-[188px] lg:pt-[169px] pb-[28px] lg:pb-6"
+>
 	<div class="flex p-[16px] flex-col items-center gap-[10px] self-stretch">
 		<AppLogo type="landing" />
 	</div>
 	<div class="flex p-[16px] justify-center items-center gap-[10px] self-stretch w-full">
-		<div class="flex-[1_0_0] text-center text-[17px] font-medium tracking-[0px] w-full">
+		<div class="lg:hidden flex-[1_0_0] text-center text-[17px] font-medium tracking-[0px] w-full">
 			{$t('welcome_mobile')}
+		</div>
+		<div
+			class="hidden lg:block flex-[1_0_0] text-center text-[17px] font-medium tracking-[0px] w-full"
+		>
+			{$t('end-to-end-encryption-collaborative-features-for-secure-and-efficient-teamwork')}
 		</div>
 	</div>
 	<div class="flex flex-col mt-auto gap-[16pt] py-4">
