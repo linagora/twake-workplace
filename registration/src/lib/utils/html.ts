1bceca7f94f42ba32a0ae2f44bf4fbf93ed3fbcf
Khaled FERJANI <kferjani@linagora.com>
2024-06-03 13:52:57 +0100

    ✨ feat(utils): added stops typing svelte action

--- registration/src/lib/utils/html.ts
+++ registration/src/lib/utils/html.ts
@@ -1,4 +1,21 @@
-export const clickOutside = (node: HTMLElement, onEventFunction: () => void) => {
+import { debounce } from '$utils/timeout';
+
+type ActionType = {
+	destroy: () => void;
+};
+
+/**
+ * Click outside svelte action.
+ *
+ * calls a handler when the user clicks outside 
+ *
+ * @param {HTMLElement} node - The node to listen to.
+ * @param {Function} onEventFunction - The function to call when the event is triggered.
+ * @example
+ *  <div use:clickOutside={closeModal} />
+ * @returns {ActionType}
+ */
+export const clickOutside = (node: HTMLElement, onEventFunction: () => void): ActionType => {
 	const handleClick = (event: Event) => {
 		const path = event.composedPath();
 
@@ -12,6 +29,32 @@ export const clickOutside = (node: HTMLElement, onEventFunction: () => void) => 
 	return {
 		destroy() {
 			document.removeEventListener('click', handleClick);
+		}
+	};
+};
+
+/**
+ * Stop typing svelte action.
+ *
+ * fires a custom event when the user stops typing after some delay.
+ *
+ * @param {HTMLElement} node - The node to listen to.
+ * @example
+ *  <input use:stopTyping on:stopTyping={typing} />
+ * @returns {ActionType}
+ */
+export const stopTyping = (node: HTMLElement): ActionType => {
+	const handler = debounce((event: Event) => {
+		if (node.contains(event.target as Node)) {
+			node.dispatchEvent(new CustomEvent('stopTyping'));
+		}
+	});
+
+	node.addEventListener('input', handler);
+
+	return {
+		destroy() {
+			node.removeEventListener('input', handler);
 		}
 	};
 };
