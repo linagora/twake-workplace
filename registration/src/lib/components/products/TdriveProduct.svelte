3facf5f1d735e9dd676f541bfdcb29bbe0b56e32
Khaled Ferjani <k2ll33d2@gmail.com>
2024-06-03 15:46:05 +0100

    🎨 make the whole product tab info clickable (#238) (#239)
    * 🎨 feat: make the whole product tab info clickable
    * ♻️ chore(products): remove nested arrow buttons

--- registration/src/lib/components/products/TdriveProduct.svelte
+++ registration/src/lib/components/products/TdriveProduct.svelte
@@ -7,7 +7,11 @@
 	};
 </script>
 
-<div class="flex flex-row space-x-5 bg-gray-100 p-5 rounded-3xl w-full">
+<button
+	on:click={open}
+	type="button"
+	class="flex flex-row space-x-5 bg-gray-100 p-5 rounded-3xl w-full cursor-pointer"
+>
 	<div class="shrink items-center justify-center">
 		<img src="/images/tdrive.svg" alt="tdrive" class="h-14 w-14" />
 	</div>
@@ -28,9 +32,8 @@
 	<button
 		aria-label="open tdrive"
 		type="button"
-		on:click={open}
 		class="flex shrink items-center justify-center"
 	>
 		<img src="/images/arrow-forward.svg" alt="next" class="w-6" />
 	</button>
-</div>
+</button>
